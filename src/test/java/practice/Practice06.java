84b7dd0c468e0ec6063d4883fb601ad9d74481f3
nurullahkaleli <nurullahkaleli@hotmail.com>
2023-03-28 15:07:46 +0300

    upload

--- src/test/java/practice/Practice06.java
+++ src/test/java/practice/Practice06.java
@@ -1,13 +1,16 @@
 package practice;
 
 import base_urls.ReqresBaseUrl;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
 
 public class Practice06 extends ReqresBaseUrl {
@@ -40,17 +43,29 @@ public class Practice06 extends ReqresBaseUrl {
         Response response = given(spec).when().get("{first}/{second}");
         response.prettyPrint();
 
+
         //Do assertion
         //1)Status code is 200
         assertEquals(200,response.statusCode());
 
-        //Print all pantone_values
-        JsonPath jsonPath = response.jsonPath();
-        List<Object> pantoneValuesList = jsonPath.getList("pantone_values");
+        //2)Print all pantone_values
+        JsonPath json = response.jsonPath();
+        List<String> pantoneValuesList = json.getList("data.pantone_value");
         System.out.println("pantoneValuesList = " + pantoneValuesList);
 
-//        List<Object> pantoneValuesList =jsonPath.getList("findAll{pantone_values}.pantone_values");
-//        System.out.println("pantoneValuesList = " + pantoneValuesList);
+        //3)Print all ids greater than 3 on the console
+        //Assert that there are 3 ids greater than 3
+
+        List<String> ids = json.getList("data.findAll{it.id>3}.id");
+        System.out.println("ids = " + ids);
+        Assert.assertEquals(3,ids.size());
+
+        //4)Print all names whose ids are less than 3 on the console
+        //Assert that the number of names whose ids are less than 3 is 2
+
+        List<String> names = json.getList("data.findAll{it.id<3}.name");
+        System.out.println("names = " + names);
+        Assert.assertEquals(2,names.size());
 
 
     }
---
+++ src/test/java/practice/Practice07.java
@@ -0,0 +1,68 @@
+package practice;
+
+import base_urls.ReqresBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+
+public class Practice07 extends ReqresBaseUrl {
+
+     /*
+        Given
+            1) https://reqres.in/api/users
+            2) {
+                "name": "morpheus",
+                "job": "leader"
+                }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+            And response body should be like {
+                                                "name": "morpheus",
+                                                "job": "leader",
+                                                "id": "496",
+                                                "createdAt": "2022-10-04T15:18:56.372Z"
+                                              }
+     */
+
+    @Test
+
+    public void practice07() {
+
+        //Set the url
+        spec.pathParams("first", "api", "second", "users");
+
+        //Set the expected the data
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("name", "morpheus");
+        expectedData.put("job", "leader");
+
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+        Response response = given(spec).contentType(ContentType.JSON).body(expectedData).when().post("{first}/{second}");
+        response.prettyPrint();
+
+        //Do Assertion
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(201, response.statusCode());
+        assertEquals(expectedData.get("name"), actualData.get("name"));
+        assertEquals(expectedData.get("job"), actualData.get("job"));
+
+
+    }
+}
