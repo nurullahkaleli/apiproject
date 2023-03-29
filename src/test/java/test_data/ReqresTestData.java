ddc2b526d124def6f5d1f73ab366c4da5aa017a7
nurullahkaleli <nurullahkaleli@hotmail.com>
2023-03-29 16:20:46 +0300

    upload

---
+++ src/test/java/test_data/ReqresTestData.java
@@ -0,0 +1,19 @@
+package test_data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReqresTestData {
+
+    public Map<String,String> reqresUsersSetUp(String name, String job){
+
+        Map<String,String> expectedData = new HashMap<>();
+        expectedData.put("name", name);
+        expectedData.put("job", job);
+
+        return expectedData;
+
+
+
+    }
+}
